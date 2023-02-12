f185e74189f3b8a1b6a67a53b548df843287b2fc
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-02-12 13:20:49 -0500

    Update PlayerHubChess.lua

--- PlayerHubChess.lua
+++ PlayerHubChess.lua
@@ -1,3 +1,12 @@
+-- ignore the flunky ass code
+
+wait(0.5)
+local bb=game:service'VirtualUser'
+game:service'Players'.LocalPlayer.Idled:connect(function()
+bb:CaptureController()
+bb:ClickButton2(Vector2.new())
+end)
+
 getgenv().LibTheme = {
     MainColor = Color3.fromRGB(45,35,35);
     BrighterMainColor = Color3.fromRGB(45,40,40);
@@ -32,6 +41,154 @@ local Button = Section.Component("Button","Delete all gates",function()
         
 local Page = Main.Page("Equip Skins","3926305904",Vector2.new(924, 204),Vector2.new(36, 36))
 
+local Button = Section.Component("Button","Bring all chess balls to you (not mine)",function()
+    local UserInputService = game:GetService("UserInputService")
+    local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
+    local Folder = Instance.new("Folder", game:GetService("Workspace"))
+    local Part = Instance.new("Part", Folder)
+    local Attachment1 = Instance.new("Attachment", Part)
+    Part.Anchored = true
+    Part.CanCollide = false
+    Part.Transparency = 1
+    local Updated = Mouse.Hit + Vector3.new(0, 5, 0)
+    local NetworkAccess = coroutine.create(function()
+        settings().Physics.AllowSleep = false
+        while game:GetService("RunService").RenderStepped:Wait() do
+            for _, Players in next, game:GetService("Players"):GetPlayers() do
+                if Players ~= game:GetService("Players").LocalPlayer then
+                    Players.MaximumSimulationRadius = 0 
+                    sethiddenproperty(Players, "SimulationRadius", 0) 
+                end 
+            end
+            game:GetService("Players").LocalPlayer.MaximumSimulationRadius = math.pow(math.huge,math.huge)
+            setsimulationradius(math.huge) 
+        end 
+    end) 
+    coroutine.resume(NetworkAccess)
+    local function ForcePart(v)
+        if v:IsA("Part") and v.Anchored == false and v.Parent:FindFirstChild("Humanoid") == nil and v.Parent:FindFirstChild("Head") == nil and v.Name ~= "Handle" then
+            Mouse.TargetFilter = v
+            for _, x in next, v:GetChildren() do
+                if x:IsA("BodyAngularVelocity") or x:IsA("BodyForce") or x:IsA("BodyGyro") or x:IsA("BodyPosition") or x:IsA("BodyThrust") or x:IsA("BodyVelocity") or x:IsA("RocketPropulsion") then
+                    x:Destroy()
+                end
+            end
+            if v:FindFirstChild("Attachment") then
+                v:FindFirstChild("Attachment"):Destroy()
+            end
+            if v:FindFirstChild("AlignPosition") then
+                v:FindFirstChild("AlignPosition"):Destroy()
+            end
+            if v:FindFirstChild("Torque") then
+                v:FindFirstChild("Torque"):Destroy()
+            end
+            v.CanCollide = false
+            local Torque = Instance.new("Torque", v)
+            Torque.Torque = Vector3.new(100000, 100000, 100000)
+            local AlignPosition = Instance.new("AlignPosition", v)
+            local Attachment2 = Instance.new("Attachment", v)
+            Torque.Attachment0 = Attachment2
+            AlignPosition.MaxForce = 9999999999999999
+            AlignPosition.MaxVelocity = math.huge
+            AlignPosition.Responsiveness = 200
+            AlignPosition.Attachment0 = Attachment2 
+            AlignPosition.Attachment1 = Attachment1
+        end
+    end
+    for _, v in next, game:GetService("Workspace"):GetDescendants() do
+        ForcePart(v)
+    end
+    game:GetService("Workspace").DescendantAdded:Connect(function(v)
+        ForcePart(v)
+    end)
+    UserInputService.InputBegan:Connect(function(Key, Chat)
+        if Key.KeyCode == Enum.KeyCode.E and not Chat then
+           Updated = Mouse.Hit + Vector3.new(0, 5, 0)
+        end
+    end)
+    spawn(function()
+        while game:GetService("RunService").RenderStepped:Wait() do
+            Attachment1.WorldCFrame = Updated
+        end
+    end)    
+    
+        end)
+
+        local Button = Section.Component("Button","Serverhop",function()
+            local PlaceID = game.PlaceId
+            local AllIDs = {}
+            local foundAnything = ""
+            local actualHour = os.date("!*t").hour
+            local Deleted = false
+            local File = pcall(function()
+                AllIDs = game:GetService('HttpService'):JSONDecode(readfile("NotSameServers.json"))
+            end)
+            if not File then
+                table.insert(AllIDs, actualHour)
+                writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+            end
+            function TPReturner()
+                local Site;
+                if foundAnything == "" then
+                    Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+                else
+                    Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+                end
+                local ID = ""
+                if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+                    foundAnything = Site.nextPageCursor
+                end
+                local num = 0;
+                for i,v in pairs(Site.data) do
+                    local Possible = true
+                    ID = tostring(v.id)
+                    if tonumber(v.maxPlayers) > tonumber(v.playing) then
+                        for _,Existing in pairs(AllIDs) do
+                            if num ~= 0 then
+                                if ID == tostring(Existing) then
+                                    Possible = false
+                                end
+                            else
+                                if tonumber(actualHour) ~= tonumber(Existing) then
+                                    local delFile = pcall(function()
+                                        delfile("NotSameServers.json")
+                                        AllIDs = {}
+                                        table.insert(AllIDs, actualHour)
+                                    end)
+                                end
+                            end
+                            num = num + 1
+                        end
+                        if Possible == true then
+                            table.insert(AllIDs, ID)
+                            wait()
+                            pcall(function()
+                                writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                                wait()
+                                game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+                            end)
+                            wait(4)
+                        end
+                    end
+                end
+            end
+            
+            function Teleport()
+                while wait() do
+                    pcall(function()
+                        TPReturner()
+                        if foundAnything ~= "" then
+                            TPReturner()
+                        end
+                    end)
+                end
+            end
+            
+            -- If you'd like to use a script before server hopping (Like a Automatic Chest collector you can put the Teleport() after it collected everything.
+            Teleport()
+                        end)
+        
+local Page = Main.Page("Equip Skins","3926305904",Vector2.new(924, 204),Vector2.new(36, 36))
 
     
    
@@ -335,4 +492,5 @@ end)
                                                                         
                                                                     game:GetService("ReplicatedStorage").Connections.SelectSkin:FireServer(unpack(args))
                                                                     end)
+                                                                        
                                                                         
