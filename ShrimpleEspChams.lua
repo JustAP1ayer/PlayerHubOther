237b2df452b20e28ada61e9892b051d21411d50f
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-03-11 17:03:11 -0500

    Create ShrimpleEspChams.lua

---
+++ ShrimpleEspChams.lua
@@ -0,0 +1,39 @@
+local Players = game:GetService("Players")
+local function addHighlight(character)
+    local highlight = Instance.new("Highlight", character)
+end
+local function onCharacterAdded(character)
+    addHighlight(character)
+    character.DescendantAdded:Connect(function(descendant)
+        if descendant:IsA("Humanoid") then
+            descendant.Died:Connect(function()
+                highlight = descendant.Parent:FindFirstChildOfClass("Highlight")
+                if highlight then
+                    highlight:Destroy()
+                end
+            end)
+        end
+    end)
+end
+for _, player in ipairs(Players:GetPlayers()) do
+    player.CharacterAdded:Connect(function(character)
+        onCharacterAdded(character)
+    end)
+    if player.Character then
+        onCharacterAdded(player.Character)
+    end
+end
+Players.PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(function(character)
+        onCharacterAdded(character)
+    end)
+end)
+Players.PlayerRemoving:Connect(function(player)
+    local character = player.Character
+    if character then
+        local highlight = character:FindFirstChildOfClass("Highlight")
+        if highlight then
+            highlight:Destroy()
+        end
+    end
+end)
