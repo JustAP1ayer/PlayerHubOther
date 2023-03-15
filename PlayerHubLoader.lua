e6d40d962f443aaa6096d1b20092056816c03fcb
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-03-14 22:51:53 -0400

    Add files via upload

---
+++ PlayerHubLoader.lua
@@ -0,0 +1,35 @@
+if game.PlaceId == 9796685905 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubSuperHeroRaceClicker.lua",true))()
+elseif game.PlaceId == 281489669 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubWizardTycoon2.lua",true))()
+elseif game.PlaceId == 147848991 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubParkourNinja.lua",true))() 
+elseif game.PlaceId == 4872321990 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/IslandsPlayerHub/main/webhook.lua",true))()
+wait(1)
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/JustAP1ayer/IslandsPlayerHub/main/IslandsPlayerHub1.6Obfuscated.lua'),true))()
+elseif game.PlaceId == 9992339729 or game.PlaceId == 12496197327 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubParkourNinja.lua",true))() 
+elseif game.PlaceId == 4246487209 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubBossFightingSimulator.lua",true))()
+elseif game.PlaceId == 70005410 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubBLOXHUNT.lua",true))()
+elseif game.PlaceId == 8507479511 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubHigherJumpEverySecond.lua",true))()
+elseif game.PlaceId == 1215581239 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PLAYERHUBDOOMSPIRE.lua",true))()
+elseif game.PlaceId == 183364845 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubSpeedRun4.lua",true))()
+elseif game.PlaceId == 7269094850 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubBSED.lua",true))()
+elseif game.PlaceId == 189707 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerhubNaturalDisaster.lua",true))()
+elseif game.PlaceId == 482742811 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubSpeedingWall.lua",true))()
+elseif game.PlaceId == 6222531507 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubChess.lua",true))()
+elseif game.PlaceId == 537413528 then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/JustAP1ayer/BABFTPLAYERHUB/main/BABFTV2.lua'),true))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubUniversal.lua",true))()
+end
