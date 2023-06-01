1e680869b81680cd6f436c3bd2318898126e5e3e
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-05-31 21:09:35 -0400

    Update autougcgamesniper.lua

--- autougcgamesniper.lua
+++ autougcgamesniper.lua
@@ -2,19 +2,27 @@
 hate was annoying
 https://discord.gg/nXERxPYUyZ
 ]]
+
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))() 
 local w = library:CreateWindow("PlayerHub UGC") -- Creates the window
 
-local b = w:CreateFolder("!    ! redblue.#4424") -- Creates the folder(U will put here your buttons,etc)
-b:Button("Infinite Yield",function()
+local t = w:CreateFolder("!    ! redblue.#4424") -- Creates the folder(U will put here your buttons,etc)
+t:Button("Infinite Yield",function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
  end)
- b:Button("My Universal Script",function()
+ t:Button("My Universal Script",function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubUniversal.lua",true))()
  end)
-local limid
+ local b = w:CreateFolder("P|ayer#6812") -- Creates the folder(U will put here your buttons,etc)
+local limid = 13596170554
 b:Box("Input Limited ID","string",function(value) -- "number" or "string"
    limid = value
+end)
+b:Button("Print Limited Information",function()
+    print("Asset ID: " ..  game:GetService("MarketplaceService"):GetProductInfo(limid).AssetId .. " \n" .. "Name: " ..  game:GetService("MarketplaceService"):GetProductInfo(limid).Name .. " \n" .. "Description: " ..  game:GetService("MarketplaceService"):GetProductInfo(limid).Description )
+end)
+b:Button("Copy Limited Information",function()
+    setclipboard("Asset ID: " ..  game:GetService("MarketplaceService"):GetProductInfo(limid).AssetId .. " \n" .. "Name: " ..  game:GetService("MarketplaceService"):GetProductInfo(limid).Name .. " \n" .. "Description: " ..  game:GetService("MarketplaceService"):GetProductInfo(limid).Description )
 end)
 local updspeed = 0
 b:Slider("Update Speed",{
@@ -27,15 +35,23 @@ end)
 b:Button("Buy Limited",function()
    game:GetService("MarketplaceService"):PromptPurchase(game.Players.LocalPlayer, tostring(limid))
 end)
-
 local limsniper = false
-b:Toggle("Toggle Auto Limited Sniper",function(bool)
-   limsniper = bool
-   while  task.wait(updspeed) and limsniper == true and limid ~= nil do
-      game:GetService("MarketplaceService"):PromptPurchase(game.Players.LocalPlayer, tostring(limid))
-      end
-   end)
 
+b:Toggle("Toggle Auto Limited Sniper V1 (AutoPrompts)", function(bool1)
+    limsniper = bool1
+    while task.wait(updspeed) and limsniper and limid ~= nil do
+        game:GetService("MarketplaceService"):PromptPurchase(game.Players.LocalPlayer, tostring(limid))
+    end
+end)
+
+
+   local limsniper1 = false
+   b:Toggle("Toggle Auto Limited Sniper V2 (Prompt when Onsale)", function(bool)
+       limsniper1 = bool
+       while task.wait(updspeed) and limsniper1 == true and game:GetService("MarketplaceService"):GetProductInfo(tonumber(limid)).IsForSale do
+           game:GetService("MarketplaceService"):PromptPurchase(game.Players.LocalPlayer, tostring(limid))
+       end
+   end)
 local gameid
 b:Box("Insert Game ID","number",function(value) -- "number" or "string"
 gameid = value
