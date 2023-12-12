d57a6f0b0adf23d648d0ffdaaf007cafae57d84d
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-12-12 16:16:45 -0500

    Update PlayerHubMML2.lua

--- PlayerHubMML2.lua
+++ PlayerHubMML2.lua
@@ -1,8 +1,8 @@
-local hook
+--[[local hook
 hook = hookfunction(game.HttpGet,function(self,url,...)
     if tostring(url) == "https://raw.githubusercontent.com/JustAP1ayer/ASalfinUiBackup-/main/OcerLibBACKUP.lua" then
         return hook(self,"https://raw.githubusercontent.com/JustAP1ayer/ASalfinUiBackup-/main/ocerlibbackupnopopup.lua",...)
     end
     return hook(self,url,...)
-end)
+end)]]--
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/PlayerHubOther/main/PlayerHubMML2loadstirng.lua",true))()
